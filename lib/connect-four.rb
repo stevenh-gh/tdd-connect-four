1a832bb4bc2638d312536c115cb0493d05252e4c
steven huynh <steven.h@live.com>
2020-11-22 17:45:00 -0800

    added #drop_piece_at method. it takes in a character and position(the row). it iterates down the grid to place the character, checking: - if bottom of grid, in which case the idx+1 is nil, then it places piece. - if the idx+1 is not empty (empty string ""), then it places piece.

--- lib/connect-four.rb
+++ lib/connect-four.rb
@@ -1,6 +1,17 @@
+require 'pry'
 class Game
   attr_accessor :grid
   def initialize(row = 6, col = 7)
-    @grid = Array.new(row) { Array.new col }
+    @grid = Array.new(row) { Array.new col, '' }
+  end
+
+  def drop_piece_at(c, pos)
+    grid.each_index do |idx|
+      if (!grid[idx + 1].nil? && !grid[idx + 1][pos].empty?) || grid[idx + 1].nil?
+        grid[idx][pos] = c
+        break
+      end
+    end
+    grid
   end
 end
