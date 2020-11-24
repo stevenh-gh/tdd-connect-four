69a59b28fe523a00c277071428c8483363f589dd
steven huynh <steven.h@live.com>
2020-11-23 16:43:51 -0800

    added first spec for #check_win method.

--- spec/connect-four_spec.rb
+++ spec/connect-four_spec.rb
@@ -15,7 +15,7 @@ describe Game do
     end
   end
   describe '#drop_piece_at' do
-    it 'takes in x/o and col pos and returns board with piece' do
+    it 'takes in x/o and col pos and returns coordinate of piece placed' do
       game = Game.new
       arr = Array.new(game.grid.length) { Array.new game.grid[0].length, '' }
       arr[5][4] = 'x'
@@ -27,8 +27,18 @@ describe Game do
       arr[5][4] = 'x'
       arr[4][4] = 'o'
       game.drop_piece_at('x', 4)
-      p game.grid
       expect(game.drop_piece_at('o', 4)).to eql([4, 4])
+    end
+  end
+  describe '#check_win' do
+    it 'checks if there\'s connect four based on coordinates given' do
+      game = Game.new
+      arr = Array.new(game.grid.length) { Array.new game.grid[0].length, '' }
+      game.drop_piece_at('x', 1)
+      game.drop_piece_at('x', 2)
+      game.drop_piece_at('x', 3)
+      coord = game.drop_piece_at('x', 4)
+      expect(game.check_win(coord)).to eql(true)
     end
   end
 end
