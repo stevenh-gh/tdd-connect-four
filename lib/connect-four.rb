d2eeed15cf918229a881557268d8ca8965b58296
steven huynh <steven.h@live.com>
2020-11-24 17:48:17 -0800

    added private method #check_win_diagonal_up checks for a connect four on a upwards diagonal slant.

--- lib/connect-four.rb
+++ lib/connect-four.rb
@@ -18,7 +18,8 @@ class Game
   end
 
   def check_win(coord)
-    check_win_horizontal(coord) || check_win_vertical(coord)
+    check_win_horizontal(coord) || check_win_vertical(coord) ||
+      check_win_diagonal_up(coord)
   end
 
   private
@@ -51,4 +52,54 @@ class Game
 
     false
   end
+
+  def check_win_diagonal_up(coord)
+    # get highest point of diagonal line
+    unless coord[0].zero?
+      x = 0
+      y = coord[1] + coord[0]
+    end
+    # iterate down
+    diagonal_arr = []
+    loop do
+      diagonal_arr << grid[x][y] unless grid[x].nil?
+      break if y.zero?
+
+      x += 1
+      y -= 1
+    end
+    char = grid[coord[0]][coord[1]]
+    count = diagonal_arr.reduce(0) do |acc, ele|
+      unless ele.nil?
+        unless ele.empty?
+          char == ele ? acc += 1 : acc = 0
+        end
+      end
+      acc
+    end
+    return true if count >= 4
+
+    false
+  end
 end
+game = Game.new
+arr = Array.new(game.grid.length) { Array.new game.grid[0].length, '' }
+game.drop_piece_at('x', 0)
+game.drop_piece_at('x', 1)
+game.drop_piece_at('o', 2)
+game.drop_piece_at('o', 3)
+game.drop_piece_at('x', 0)
+game.drop_piece_at('o', 1)
+game.drop_piece_at('x', 2)
+game.drop_piece_at('x', 3)
+game.drop_piece_at('x', 1)
+game.drop_piece_at('o', 2)
+game.drop_piece_at('o', 3)
+game.drop_piece_at('x', 2)
+game.drop_piece_at('x', 3)
+coord = game.drop_piece_at('x', 3)
+game.grid.each do |ele|
+  ele.each { |pi| print "[#{pi.empty? ? ' ' : pi}]" }
+  puts
+end
+p game.check_win(coord)
